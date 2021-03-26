db2bfd8a393c0fea2b3422684516da4c947d4ba8
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-26 19:40:25 -0300

    added operator model test

--- backend/src/app/model/operator.js
+++ backend/src/app/model/operator.js
@@ -1,6 +1,16 @@
 import { model, Schema } from 'mongoose';
 
-const Operator = Schema({
+const operatorSchema = Schema({
     name: String,
     clients: Array,
 })
+
+const operatorModel = model('operator', operatorSchema);
+
+const createOperator = async() => {
+    const operator = new operatorModel({ name: 'Operator 1', clients: ['Client 1', 'Client 2'] });
+    await operator.save().then(res => console.log(res)).catch(err => { err })
+    return operator;
+}
+
+export default createOperator();
--- backend/src/database/mongoose_connection.js
+++ backend/src/database/mongoose_connection.js
@@ -12,5 +12,4 @@ const { DATABASE_USER, DATABASE_PASSWORD, DATABASE_URI } = process.env;
 
 // module.exports = { model, Schema };
 
-module.exports = { URI: `mongodb+srv://pacheck:qaro41kine@operadores.dy2uj.mongodb.net`}
-// module.exports = { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` };
+module.exports = { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` };
