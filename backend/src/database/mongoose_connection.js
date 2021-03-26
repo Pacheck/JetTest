bbf15950cb204386e1183e686b2694265d9e41c7
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-26 17:28:53 -0300

    testing new export

--- backend/src/database/mongoose_connection.js
+++ backend/src/database/mongoose_connection.js
@@ -12,5 +12,5 @@ const { DATABASE_USER, DATABASE_PASSWORD, DATABASE_URI } = process.env;
 
 // module.exports = { model, Schema };
 
-module.exports = { URI: `mongodb+srv://${pacheck}:${qaro41kine}@${operadores.dy2uj.mongodb.net/kitty}`}
+module.exports = { URI: `mongodb+srv://pacheck:qaro41kine@operadores.dy2uj.mongodb.net/kitty`}
 // module.exports = { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` };
