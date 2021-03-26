9389d0d5cdd2aa4908e5b0fc2ff49ea3406f2d79
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-26 20:47:19 -0300

    fixes

--- backend/src/database/mongoose_config.js
+++ backend/src/database/mongoose_config.js
@@ -2,5 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { DATABASE_USER, DATABASE_PASSWORD, DATABASE_URI } = process.env;
+console.log('mongoose config #######', DATABASE_USER, DATABASE_URI)
 
-module.exports = { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` };
+// module.exports = { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` };
+export default { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` }
