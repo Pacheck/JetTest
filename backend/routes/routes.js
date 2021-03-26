f36cee5ea3e47bf95ea8c1a56c69835e5929ac0b
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-26 19:40:33 -0300

    created route to operator model test

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,22 +1,15 @@
 import express from 'express';
 const routes = express.Router();
 import { model, Schema } from 'mongoose';
+import OperatorModel from '../src/app/model/operator';
+
 
 routes.get("/", function(req, res) {
   return res.send("Minha primeira rota!");
 });
 
 routes.get("/teste", (req, res) => {
-  const kittySchema = new Schema({
-    name: String,
-    age: String,
-});
-
-  const Cat = model('Cat', kittySchema);
-
-  const kitty = new Cat({ name: 'Negão', age: '2 years' });
-  kitty.save().then(() => console.log('meow')).catch(err => console.log({ err }));
-
+  OperatorModel();
   return res.send('criado')
 })
 
