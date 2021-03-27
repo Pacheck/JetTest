9c7328c02d09bda3114d9fd6d2c74f205662b639
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-27 01:46:56 -0300

    fix

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -10,6 +10,6 @@ routes.get("/", function(req, res) {
 
 routes.get("/operators", OperatorController.index);
 
-routes.post("operators", OperatorController.store)
+routes.post("/operators", OperatorController.store)
 
 module.exports = routes;
