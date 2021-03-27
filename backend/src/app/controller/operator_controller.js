4f39af0904446069605e2b0a7df67385383dcccb
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-27 17:02:00 -0300

    testing res status code

--- backend/src/app/controller/operator_controller.js
+++ backend/src/app/controller/operator_controller.js
@@ -5,28 +5,28 @@ class OperatorController {
     async store(request, response){
         try{
             await Operator.create(req.body)
-            .then(res => response.status(201).json({ message: 'Resource created! '}))
+            .then(res => response.status(201).send({ message: 'Resource created! '}))
         }catch(err){
-            response.status(400).json({ message: 'Error' })
+            response.status(400).send({ message: 'Error' })
         }
     }
 
     async index(request, response){
         await Operator.find({})
-        .then(res => res.status(201).json({ message: 'Content created! '}))
-        .catch(err => err.status(400).json({ message: 'Content not found' }));
+        .then(res => res.status(201).send({ message: 'Content created! '}))
+        .catch(err => err.status(400).send({ message: 'Content not found' }));
     }
 
     async delete(request, response){
         await Operator.deleteOne({})
-        .then(res => response.status(200).json({ message: 'Resource deleted!' }))
-        .catch(err => response.status(400).json({ message: 'Content error'}));
+        .then(res => response.status(200).send({ message: 'Resource deleted!' }))
+        .catch(err => response.status(400).send({ message: 'Content error'}));
     }
 
     async update(request, response){
         await Operator.updateOne({})
-        .then(res => response.status(200).json({ message: 'Resource updated! '}))
-        .catch(err => response.status(204).json({ message: 'No content found '}));
+        .then(res => response.status(200).send({ message: 'Resource updated! '}))
+        .catch(err => response.status(204).send({ message: 'No content found '}));
     }
 }
 
