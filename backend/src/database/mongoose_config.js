17d7c20626150ca465204f524998d77b31aef619
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-27 00:36:33 -0300

    add mock data to heroku

--- backend/src/database/mongoose_config.js
+++ backend/src/database/mongoose_config.js
@@ -3,8 +3,4 @@ dotenv.config();
 
 const { DATABASE_USER, DATABASE_PASSWORD, DATABASE_URI } = process.env;
 
-// DATABASE_USER=pacheck
-// DATABASE_PASSWORD=qaro41kine
-// DATABASE_URI=operadores.dy2uj.mongodb.net/operators
-
 export default { URI: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URI}` }
