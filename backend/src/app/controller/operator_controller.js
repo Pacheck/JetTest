8ad87b7533947bde0b887ccbdbb7147372d65b2f
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-27 21:34:57 -0300

    fixed all routes logic http

--- backend/src/app/controller/operator_controller.js
+++ backend/src/app/controller/operator_controller.js
@@ -1,32 +1,49 @@
+import { v4 as uuidv4 } from 'uuid';
 import Operator from '../model/operator';
 
 class OperatorController { 
-
-    async store(request, response){
-        try{
-            await Operator.create(req.body)
-            .then(res => response.status(201).send({ message: 'Resource created! '}))
+    
+    async index(request, response){
+        try {
+            const operator = await Operator.find({});
+            return response.status(200).json(operator);
         }catch(err){
-            response.status(400).send({ message: 'Error' })
+            return response.status(400).json({ message: 'Content not found' }) 
         }
     }
 
-    async index(request, response){
-        await Operator.find({})
-        .then(res => res.status(201).send({ message: 'Content created! '}))
-        .catch(err => err.status(400).send({ message: 'Content not found' }));
+    async store(request, response){
+        const { body } = request;
+        body.id = uuidv4();
+        try{
+           const operator = await Operator.create(body);
+           response.status(201).json(operator);
+        }catch(err){
+            response.status(400).json({ message: 'Error creating new operator' })
+        }
     }
 
     async delete(request, response){
-        await Operator.deleteOne({})
-        .then(res => response.status(200).send({ message: 'Resource deleted!' }))
-        .catch(err => response.status(400).send({ message: 'Content error'}));
+        const { id } = request.params;
+        try {
+            const operator = await Operator.deleteOne({ id });
+            return response.status(200).json(operator);
+        } catch (err) {
+            return response.status(400).json({ message: 'Content error'})
+        }
     }
 
     async update(request, response){
-        await Operator.updateOne({})
-        .then(res => response.status(200).send({ message: 'Resource updated! '}))
-        .catch(err => response.status(204).send({ message: 'No content found '}));
+        const { id } = request.params;
+        const { body } = request;
+        console.log(id);
+        console.log(body);
+        try {
+            const operator = await Operator.updateOne({ id }, body)
+            response.status(200).json(operator)
+        } catch (err) {
+            response.status(204).json({ message: 'No content found '})
+        }
     }
 }
 
