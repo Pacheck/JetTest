7b740b2790ad84b85a29b0f5e8aeb74d1713145c
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-27 21:34:20 -0300

    removed id off the schema

--- backend/src/app/model/operator.js
+++ backend/src/app/model/operator.js
@@ -1,11 +1,14 @@
 import { model, Schema } from 'mongoose';
 
 const operatorSchema = Schema({
+    id: {
+        type: String,
+        required: true,
+    },
     name: {
         type: String,
         required: true,
     },
-
     clients: {
         type: Array,
         required: true,
