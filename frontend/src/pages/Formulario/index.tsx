cee1cebc868bc30f2e7bf93a14f55dc9980eca54
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-30 01:54:39 -0300

    initialized pages

---
+++ frontend/src/pages/Formulario/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Formulario = () => {
+  return <div>Hello</div>;
+};
+
+export default Formulario;
--- frontend/src/pages/Listagem/index.tsx
+++ frontend/src/pages/Listagem/index.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CSVReader from "react-csv-reader";
+import customAPI from "../../hooks/useAPI";
+import { handleSequence } from "../../helper/handleSequence";
+
+import "./index.css";
 
 const Listagem = () => {
-  return <div>Hello, Listagem</div>;
+  const [csvData, setCsvData] = useState([]);
+  const [operators, setOperators, loadOperators] = customAPI();
+
+  const handleClientsToOperators = async () => {
+    await loadOperators("/operators");
+    const result = handleSequence(operators, csvData);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    handleClientsToOperators();
+  }, []);
+
+  return (
+    <>
+      <CSVReader
+        cssInputClass="colorTransparent"
+        onFileLoaded={(data: any) => {
+          // any[] type is recommended by csvreader module on npm
+          setCsvData(data);
+        }}
+      />
+      <h2 onClick={handleClientsToOperators}>rs</h2>
+
+      {operators.map((operator) => (
+        <h2 key={operator.id}>{operator.name}</h2>
+      ))}
+    </>
+  );
 };
 
 export default Listagem;
